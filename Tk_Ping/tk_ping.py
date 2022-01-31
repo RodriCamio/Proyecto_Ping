b806711e56fbda44666cc8cc16bdc1cfed7fec1d
Rodrigo Camio <rodricamio94@gmail.com>
2022-01-31 16:01:27 -0300

    avance un poco mas con tk

--- Tk_Ping/tk_ping.py
+++ Tk_Ping/tk_ping.py
@@ -1,28 +1,32 @@
 import tkinter as tk
-
+#Crear la ventana.
 ventana = tk.Tk()
-ventana.geometry('900x600')
-ventana.resizable(0,0) #nos permite redimencional la pagina. Los parametros son boobleanos, de manera que True es 1 y False es 0
-
+#Establecer un titulo.
 ventana.title('PROGRAMA DE PINES v1.0 BETA')
-
-titulo = tk.Label(ventana,text= 'Programa de Pines', font=('Calibri',20))
-titulo.place(x=100,y=70)
-
+#Establecer un tamaño.
+ventana.config(width=900, height=600)
+#Bloquear el redireccionamiento.
+ventana.resizable(0,0) #nos permite redimencional la pagina. Los parametros son boobleanos, de manera que True es 1 y False es 0
 nombre = tk.Label(ventana,text= 'Nombre:', font=('Calibri',16))
 nombre.place(x=300,y=260)
-
-ingreso_nombre = tk.Entry(ventana)
-ingreso_nombre.place(x=400,y=265)
-
+#Crear un boton.
 boton_siguiente = tk.Button(ventana, text='Siguiente')
-boton_siguiente.pack(anchor='ne')
+#Le doy un tamaño y una posicion.
+boton_siguiente.place(x=700, y=500, width=100, height=25)
+#Crear un caja de texto.
+ingreso_nombre = tk.Entry(ventana)
+#Le doy un tamaño y una posicion.
+ingreso_nombre.place(x=400,y=265, width=200, height=25)
+#Agregamos una etiqueta con titulo.
+titulo = tk.Label(ventana,text= 'Programa de Pines', font=('Calibri',20))#le ajustamos la fuente y el tamaño.
+#Establecemos ubicacion de la etiqueta.
+titulo.place(x=100,y=70)
 
 
 
 
 
-
-ventana.mainloop()
+#Mostrar ventana
+ventana.mainloop() #corta el codigo hasta que la ventana se cierre. 
 
 
